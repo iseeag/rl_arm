3273691beb46efd2250cffec1857ff2c7b9d69d7
Maximillion <iseeag@hotmail.com>
2019-11-27 10:08:21 +0800

    refactor StateDataset for simpler logic

--- interface.py
+++ interface.py
@@ -71,51 +71,33 @@ def make_transfrom_f(include_torque=True):
     return lambda d: transform_env_state_to_nn_state(d, include_torque)
 
 
-
-
-
-
 class StateDataset(Dataset):
-    def __init__(self, env_instance: EnvironmentState, transform_f, size=3500000, skip_step=0, include_torque=True):
+    def __init__(self, env_instance: EnvironmentState, transform_f,
+                 size=3500000, skip_step=0, random_torque=True, include_torque=True):
         self.size = size
         self.skip_step = skip_step
         self.env = env_instance
         self.transform_f = transform_f
+        self.random_torque = random_torque
         self.include_torque = include_torque
         # initiate environment
         for _ in range(20):
-            self.env.step(random_movement=True)
+            self.env.step(random_torque=True)
 
     def __len__(self):
         return self.size
 
     def __getitem__(self, idx):
-        s0 = None
-        # not including torques in array
-        while not self.include_torque:
-            s0 = self.env.get_current_state()
-            torques_record = s0['torques']
-            if torques_record is not None:
-                self.env.step(random_movement=True)
-                continue # found torques start all over again
-            # skip steps
-            for i in range(self.skip_step):
-                self.env.step(random_movement=True)
-                torques_record = self.env.get_current_state()['torques']
-                if torques_record is not None: break # found torques breakout for loop
-            if torques_record is not None: continue # found torques start over again
+        # set random torque
+        if self.random_torque:
+            self.env.torque_random_set() # set torque
+        if not self.include_torque:
+            self.env.step() # get rid of torque
+        s0 = self.env.get_current_state()
+        for i in range(self.skip_step):
+            self.env.step()
 
-            # no torques found, breakout while loop
-            break
-
-        # including torques in array
-        if self.include_torque:
-            self.env.torque_random_set()
-            s0 = self.env.get_current_state()
-            for i in range(self.skip_step):
-                self.env.step()
-
-        self.env.step(random_movement= not self.include_torque) # random movement when not include torques
+        self.env.step(random_torque=False) # random movement when not include torques
         s1 = self.env.get_current_state()
 
         return {'s0': self.transform_f(s0),
